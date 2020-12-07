5fc4af5724f994becbe2bff94c4987dc95850840
Allen Kuo <g01.webapp@gmail.com>
2020-12-07 14:23:46 +0800

    add :FacadeUpStreamOrder

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ConsoleApp
 {
@@ -11,18 +13,53 @@ namespace ConsoleApp
 	{
 		static void Main(string[] args)
 		{
-			IOrderAdapter adapter = new JsonOrderAdapter();
+			FacadeUpStreamOrder facade = new FacadeUpStreamOrder();
+
+			IOrderAdapter adapter = new JsonOrderAdapter(facade);
 
 			Order[] orders = new OrderService(adapter).ProcessOrders();
 		}
 	}
 
-	internal class JsonOrderAdapter : IOrderAdapter
+	public class FacadeUpStreamOrder
 	{
-		public Order[] GetOrders()
+		public UpStreamOrder[] GetOrder()
 		{
 			throw new NotImplementedException();
 		}
+	}
+
+	internal class JsonOrderAdapter : IOrderAdapter
+	{
+		private FacadeUpStreamOrder facade;
+
+		public JsonOrderAdapter(FacadeUpStreamOrder facade)
+		{
+			this.facade = facade;
+		}
+		public Order[] GetOrders()
+		{
+			// UpStreamOrder[] orders = facade.GetOrder();
+			throw new NotImplementedException();
+		}
+	}
+
+	public class UpStreamOrder
+	{
+		public int Code { get; set; }
+
+		[JsonProperty("Orders")]
+		public UpStreamOrderItem[] Items { get; set; }
+	}
+
+	[JsonObject("UpStreamOrderItem")]
+	public class UpStreamOrderItem
+	{
+		[JsonProperty("FruitName")]
+		public string FruitName { get; set; }
+
+		[JsonProperty("3CName")]
+		public string TechProductName { get; set; }
 	}
 
 	public interface IOrderAdapter
