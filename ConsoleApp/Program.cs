2e2a36b4f146064c0fe1a27c58dec4949f09027a
Allen Kuo <g01.webapp@gmail.com>
2020-12-07 14:10:06 +0800

    add:OrderService

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Order[] orders = OrderService.ProcessOrders();
+		}
+	}
+
+	public class OrderService
+	{
+		public static Order[] ProcessOrders()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class Order
+	{
+		public int Code;
+		public OrderItems[] Items { get; set; }
+	}
+
+	public class OrderItems
+	{
+		public string ProductName { get; set; }
+	}
+}
